49adfcae2a724270d84e4c9208002ae4538cd190
Jeffrey Martin <jeffreymartin@Jeffreys-MacBook-Pro.local>
2020-07-20 12:21:40 -0600

    moving things around for Github Pages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./components/App";
+import App from "./src/components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
