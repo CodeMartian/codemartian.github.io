499350b0143880da699e888e7c199afaa14ed164
CodeMartian <jeffmmartin94@gmail.com>
2020-07-28 15:04:17 -0600

    Updates

---
+++ precache-manifest.e15cd3f6320e86c48880899b6411da8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e425b2aba4a5104b4299813cbc87bf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8636b76e35e7e024c94d",
+    "url": "/static/css/main.ce8488d2.chunk.css"
+  },
+  {
+    "revision": "cf03444c65d43f29c9a5",
+    "url": "/static/js/2.1d2ea59c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d2ea59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8636b76e35e7e024c94d",
+    "url": "/static/js/main.119216cd.chunk.js"
+  },
+  {
+    "revision": "d793d123cf4fa0196d3a",
+    "url": "/static/js/runtime-main.69d5fb53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
