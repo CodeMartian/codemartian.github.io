e66ab75726b53bb75f949925929d8c275a6d0cfd
Jeffrey Martin <jeffreymartin@Jeffreys-MacBook-Pro.local>
2020-07-28 14:50:31 -0600

    Sets header max width to 100

---
+++ build/precache-manifest.6612fcaa1fa666510cec9c16c3ce1040.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11891db0fed57b51e9adfd5ab0c20614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbcc2dda7e9a6ee8c8ae",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf03444c65d43f29c9a5",
+    "url": "/static/js/2.1d2ea59c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d2ea59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcc2dda7e9a6ee8c8ae",
+    "url": "/static/js/main.ac5148b5.chunk.js"
+  },
+  {
+    "revision": "d793d123cf4fa0196d3a",
+    "url": "/static/js/runtime-main.69d5fb53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/components/AboutPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AboutPage = () => {
-  return <h1>About World!</h1>;
-};
-
-export default AboutPage;
